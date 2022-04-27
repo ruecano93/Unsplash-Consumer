30b6059f91e024c9d500f58d85eddb914295d785
manueljgq93 <manueljgq@ext.zara.com>
2022-04-27 20:49:23 +0200

    initial commit

---
+++ src/main/java/com/gamero/unsplashconsumer/UnsplashConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.gamero.unsplashconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsplashConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsplashConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamero/unsplashconsumer/UnsplashConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamero.unsplashconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsplashConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
